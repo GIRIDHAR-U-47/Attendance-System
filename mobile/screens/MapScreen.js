d39f2aafbef0ac0a98ae051c309f8551c97c04f6
Giridhar <giridharuppili2004@gmail.com>
2026-05-29 17:27:38 +0530

    Update MapScreen.js

--- mobile/screens/MapScreen.js
+++ mobile/screens/MapScreen.js
@@ -89,9 +89,8 @@ export default function MapScreen({ route }) {
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
   map: { width: '100%', height: '100%' },
-  text: { fontSize: 18 }
+  text: { fontSize: 19 }
 });
